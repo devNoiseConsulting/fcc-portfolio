e8d9314acf674ab4b17cc6cc189c2de80582bfc4
Michael J. Flynn <flynnmj@devnoise.com>
2017-06-24 22:50:10 -0400

    Adding screenshots

---
+++ screenshots/convertScreenshots.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Firefox screenshot commands
+
+# screenshot Tribute-Page --fullpage
+# screenshot Personal-Portfolio-Webpage --fullpage
+
+# screenshot Random-Quote-Machine --fullpage
+# screenshot Show-the-Local-Weather --fullpage
+# screenshot Wikipedia-Viewer --fullpage
+# screenshot Use-the-Twitchtv-JSON-API --fullpage
+
+# screenshot JavaScript-Calculator --fullpage
+# screenshot Pomodoro-Clock --fullpage
+# screenshot Tic-Tac-Toe-Game --fullpage
+# screenshot Simon-Game --fullpage
+
+# screenshot Markdown-Previewer --fullpage
+# screenshot Camper-Leaderboard --fullpage
+# screenshot Game-of-Life --fullpage
+# screenshot Recipe-Box --fullpage
+# screenshot Roguelike-Dungeon-Crawler-Game --fullpage
+
+# screenshot Visualize-Data-with-a-Bar-Chart --fullpage
+# screenshot Visualize-Data-with-a-Scatterplot-Graph --fullpage
+# screenshot Visualize-Data-with-a-Heat-Map --fullpage
+# screenshot Show-National-Contiguity-with-a-Force-Directed-Graph --fullpage
+# screenshot Map-Data-Across-the-Globe --fullpage
+
+# screenshot Timestamp-Microservice --fullpage
+# screenshot Request-Header-Parser-Microservice --fullpage
+# screenshot URL-Shortener-Microservice --fullpage
+# screenshot Image-Search-Abstraction-Layer --fullpage
+# screenshot File-Metadata-Microservice --fullpage
+
+
+convert ./screenshots/Tribute-Page.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Tribute-Page.png
+convert ./screenshots/Personal-Portfolio-Webpage.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Personal-Portfolio-Webpage.png
+
+convert ./screenshots/Random-Quote-Machine.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Random-Quote-Machine.png
+convert ./screenshots/Show-the-Local-Weather.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Show-the-Local-Weather.png
+convert ./screenshots/Wikipedia-Viewer.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Wikipedia-Viewer.png
+convert ./screenshots/Use-the-Twitchtv-JSON-API.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Use-the-Twitchtv-JSON-API.png
+
+convert ./screenshots/JavaScript-Calculator.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/JavaScript-Calculator.png
+convert ./screenshots/Pomodoro-Clock.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Pomodoro-Clock.png
+convert ./screenshots/Tic-Tac-Toe-Game.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Tic-Tac-Toe-Game.png
+convert ./screenshots/Simon-Game.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Simon-Game.png
+
+convert ./screenshots/Markdown-Previewer.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Markdown-Previewer.png
+convert ./screenshots/Camper-Leaderboard.png -resize "640" -gravity North -crop 640x480+0+0 +repage -background white -gravity center -extent "640x480" ./images/Camper-Leaderboard.png
+convert ./screenshots/Game-of-Life.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Game-of-Life.png
+convert ./screenshots/Recipe-Box.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Recipe-Box.png
+convert ./screenshots/Roguelike-Dungeon-Crawler-Game.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Roguelike-Dungeon-Crawler-Game.png
+
+convert ./screenshots/Visualize-Data-with-a-Bar-Chart.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Visualize-Data-with-a-Bar-Chart.png
+convert ./screenshots/Visualize-Data-with-a-Scatterplot-Graph.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Visualize-Data-with-a-Scatterplot-Graph.png
+convert ./screenshots/Visualize-Data-with-a-Heat-Map.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Visualize-Data-with-a-Heat-Map.png
+convert ./screenshots/Show-National-Contiguity-with-a-Force-Directed-Graph.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Show-National-Contiguity-with-a-Force-Directed-Graph.png
+convert ./screenshots/Map-Data-Across-the-Globe.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Map-Data-Across-the-Globe.png
+
+convert ./screenshots/Timestamp-Microservice.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Timestamp-Microservice.png
+convert ./screenshots/Request-Header-Parser-Microservice.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Request-Header-Parser-Microservice.png
+convert ./screenshots/URL-Shortener-Microservice.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/URL-Shortener-Microservice.png
+convert ./screenshots/Image-Search-Abstraction-Layer.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/Image-Search-Abstraction-Layer.png
+convert ./screenshots/File-Metadata-Microservice.png -resize "640" -gravity North -crop 640x480\+0\+0 \+repage -background white -gravity center -extent "640x480" ./images/File-Metadata-Microservice.png
